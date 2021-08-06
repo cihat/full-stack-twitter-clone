3ae96d1d64fcb11eececb2e69e75f40489e7d4be
cihat <cihat02020202@gmail.com>
2021-08-06 17:58:09 +0300

    Add routes for each pages

---
+++ src/routes/user.js
@@ -0,0 +1,44 @@
+const { userDatabase, tweetDatabase } = require("../database")
+const flatted = require("flatted")
+
+const router = require("express").Router()
+
+router.get("/", async (req, res) => {
+  const users = await userDatabase.load()
+
+  res.render("users", { users })
+})
+
+// router.post("/", async (req, res) => {
+//   const passenger = await userDatabase.insert(req.body)
+
+//   res.send(passenger)
+// })
+
+// router.delete("/:passengerId", async (req, res) => {
+//   await userDatabase.removeBy("id", req.params.passengerId)
+
+//   res.send("OK")
+// })
+
+// router.get("/:passengerId", async (req, res) => {
+//   const passenger = await userDatabase.find(req.params.passengerId)
+//   if (!passenger) return res.status(404).send("Cannot find passenger")
+//   res.render("passenger", { passenger })
+// })
+
+// router.post("/:passengerId/bookings", async (req, res) => {
+//   const { passengerId } = req.params
+//   const { driverId, origin, destination } = req.body
+
+//   const passenger = await userDatabase.find(passengerId)
+//   const driver = await tweetDatabase.find(driverId)
+
+//   passenger.book(driver, origin, destination)
+
+//   await userDatabase.update(passenger)
+
+//   res.send("OK")
+// })
+
+module.exports = router
