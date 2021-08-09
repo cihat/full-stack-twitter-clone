03be7048cf65b4d6d2ccf3f2a0ad555da26321c9
cihat <cihat02020202@gmail.com>
2021-08-09 18:40:20 +0300

    Add user services in user class

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,25 @@
 const BaseService = require("./base-service")
 const User = require("../models/user")
+const tweetService = require("./tweet-service")
 
-class UserService extends BaseService {}
+class UserService extends BaseService {
+  async findByName(name) {
+    return this.findBy("name", name)
+  }
+
+  async tweet(userId, tweetId) {
+    const user = await this.find(userId)
+    // const tweet = await tweetService.find(tweetId)
+
+    const tweet = await tweetService.insert({
+      user,
+      tweet,
+    })
+    user?.twwets.push(tweet)
+    await user.save()
+
+    return tweet
+  }
+}
 
 module.exports = new UserService(User)
