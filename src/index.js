19d9e12fa02ebfbf94ad8ba1f1c9bd58a727df16
cihat <cihat02020202@gmail.com>
2021-08-11 15:19:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const port = 3001
 const bodyParser = require("body-parser")
 const indexRouter = require("./routes/index")
 const usersRouter = require("./routes/users")
@@ -14,6 +13,4 @@ app.set("view engine", "pug")
 app.use("/", indexRouter)
 app.use("/users", usersRouter)
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+module.exports = app
--- src/mongo-connection.js
+++ src/mongo-connection.js
@@ -8,5 +8,5 @@ mongoose.connect("mongodb://localhost:27017/twitter-clone", {
 const db = mongoose.connection
 db.on("error", console.error.bind(console, "connection error:"))
 db.once("open", function () {
-  console.log("we are connected to mongodb!🤪")
+  console.log("we are connected to mongoDB!!!🤪")
 })
