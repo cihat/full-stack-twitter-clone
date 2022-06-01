8eb166c30b650d23b1f18ff09c6e0aa7bb43e5e8
cihat <cihat02020202@gmail.com>
2022-06-02 01:56:54 +0300

    Add passport, sanitize, celebrate, connect-mongo, express-session packages for auth and security

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,82 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const authRouter = require('./routes/auth')
+
+const { mongoose } = require('./mongo-connection')
+const logger = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const cookieParser = require('cookie-parser')
+const passport = require('passport')
+const User = require('./models/user')
+const sanitize = require('express-mongo-sanitize').sanitize
+const { errors } = require('celebrate')
+
 const cors = require('cors')
-require('./mongo-connection')
 
 const app = express()
-app.use(bodyParser.json())
-app.set('views', __dirname + '/views')
-app.use(cors())
 
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      stringify: false
+    }),
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+      sameSite: process.env.NODE_ENV == 'production' && 'none',
+      secure: process.env.NODE_ENV == 'production'
+    }
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+// view engine setup
+app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(__dirname + '/public'))
+app.use(cors())
+
+app.all('*', (req, res, next) => {
+  req.body = sanitize(req.body)
+  req.headers = sanitize(req.headers)
+  req.params = sanitize(req.params)
+
+  next()
+})
+
+app.use('/api/', indexRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/auth', authRouter)
+
+app.use(errors())
+
+app.use((err, req, res, next) => {
+  const error = {
+    status: err.status || 500,
+    message: err.message
+  }
+
+  if (req.app.get('env' === 'development')) {
+    error.stack = err.stack
+  }
+
+  res.status(error.status)
+
+  res.send(error)
+})
 
 module.exports = app
