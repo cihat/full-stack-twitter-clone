8b053560a46606a3ef65847d690b24b5e9fe723a
cihat <cihat02020202@gmail.com>
2022-06-02 01:54:28 +0300

    Update project main strcuture

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,12 +2,26 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
+import store, { store_ } from './store'
+
+import AuthLayout from './layouts/AuthLayout.vue'
+import DefaultLayout from './layouts/DefaultLayout.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+Vue.component('auth-layout', AuthLayout)
+Vue.component('default-layout', DefaultLayout)
+
+Vue.config.productionTip = false
+
+async function main() {
+  let storeInstance = await store()
+
+  new Vue({
+    router: router(storeInstance),
+    store: storeInstance,
+    render: h => h(App)
+  }).$mount('#app')
+}
+
+main()
