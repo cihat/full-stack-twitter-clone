8782554956231541f52a321ffd7077e679fea9b5
cihat <cihat02020202@gmail.com>
2022-06-03 06:33:08 +0300

    Add `click-outside` for all popups

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,6 +14,22 @@ Vue.component('default-layout', DefaultLayout)
 
 Vue.config.productionTip = false
 
+Vue.directive('click-outside', {
+  bind: function(el, binding, vnode) {
+    el.clickOutsideEvent = function(event) {
+      // here I check that click was outside the el and his children
+      if (!(el == event.target || el.contains(event.target))) {
+        // and if it did, call method provided in attribute value
+        vnode.context[binding.expression](event)
+      }
+    }
+    document.body.addEventListener('click', el.clickOutsideEvent)
+  },
+  unbind: function(el) {
+    document.body.removeEventListener('click', el.clickOutsideEvent)
+  }
+})
+
 async function main() {
   let storeInstance = await store()
 
