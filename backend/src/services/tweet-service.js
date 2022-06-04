3b2618d7f67dc81f5c69ed60b1c8fd47939a6aec
cihat <cihat02020202@gmail.com>
2022-06-05 02:25:01 +0300

    Update tweet service

--- backend/src/services/tweet-service.js
+++ backend/src/services/tweet-service.js
@@ -3,40 +3,48 @@ const Tweet = require('../models/tweet')
 const userService = require('./user-service')
 
 class TweetService extends BaseService {
-  async tweet(userId, body) {
-    const user = await userService.find(userId)
-    const tweet = await this.insert({ user, body })
-    user.tweets.unshift(tweet)
-    await user.save()
-
-    return tweet
+  async findAll() {
+    return this.load()
   }
 
-  async like(userId, likeTweetId) {
-    const tweet = await this.find(likeTweetId)
-    const user = await userService.find(userId)
-
-    user.likedTweets.push(tweet)
-    tweet.likes.push(user)
-
-    await tweet.save()
-    await user.save()
-
-    return user
+  async loadWithLimit(limit) {
+    return this.loadLimit(limit)
   }
 
-  async retweet(userId, retweetId) {
-    const user = await userService.find(userId)
-    const tweetToRetweet = await this.find(retweetId)
+  // async tweet(userId, body) {
+  //   const user = await userService.find(userId)
+  //   const tweet = await this.insert({ user, body })
+  //   user.tweets.unshift(tweet)
+  //   await user.save()
 
-    user.retweets.push(tweetToRetweet)
-    tweetToRetweet.retweets.push(user.handle)
+  //   return tweet
+  // }
 
-    await user.save()
-    await tweetToRetweet.save()
+  // async like(userId, likeTweetId) {
+  //   const tweet = await this.find(likeTweetId)
+  //   const user = await userService.find(userId)
 
-    return user
-  }
+  //   user.likedTweets.push(tweet)
+  //   tweet.likes.push(user)
+
+  //   await tweet.save()
+  //   await user.save()
+
+  //   return user
+  // }
+
+  // async retweet(userId, retweetId) {
+  //   const user = await userService.find(userId)
+  //   const tweetToRetweet = await this.find(retweetId)
+
+  //   user.retweets.push(tweetToRetweet)
+  //   tweetToRetweet.retweets.push(user.handle)
+
+  //   await user.save()
+  //   await tweetToRetweet.save()
+
+  //   return user
+  // }
 }
 
 module.exports = new TweetService(Tweet)
