92240a1c3907fa62c578f779bf820e37149dd5a9
cihat <cihat02020202@gmail.com>
2022-06-05 02:24:38 +0300

    Update tweet model

--- backend/src/models/tweet.js
+++ backend/src/models/tweet.js
@@ -1,26 +1,63 @@
 const mongoose = require('mongoose')
 const autopopulate = require('mongoose-autopopulate')
 
-const TweetSchema = new mongoose.Schema({
-  body: String,
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    autopopulate: true
-  },
-  createdAt: Date,
-  originalTweet: String,
-  replies: [],
-  likes: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
+const TweetSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 140
+    },
+    author: {
+      type: 'ObjectId',
       ref: 'User',
-      autopopulate: true
-    }
-  ],
-  retweets: [],
-  attachments: []
-})
+      autopopulate: {
+        maxDepth: 2
+      },
+      required: true
+    },
+    createdAt: Date,
+    originalTweet: String,
+    replies: [
+      {
+        type: 'ObjectId',
+        ref: 'Tweet',
+        autopopulate: {
+          maxDepth: 2
+        }
+      }
+    ],
+    likes: [
+      {
+        type: 'ObjectId',
+        ref: 'User',
+        autopopulate: {
+          maxDepth: 2
+        }
+      }
+    ],
+    retweets: [
+      {
+        type: 'ObjectId',
+        ref: 'User',
+        autopopulate: {
+          maxDepth: 2
+        }
+      }
+    ],
+    quoteTweets: [
+      {
+        type: 'ObjectId',
+        ref: 'Tweet',
+        autopopulate: {
+          maxDepth: 2
+        }
+      }
+    ]
+  },
+  { timestamps: true }
+)
 
 TweetSchema.plugin(autopopulate)
 
