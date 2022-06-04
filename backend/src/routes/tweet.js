576814d4d5b2c9597edfe1d09a13c0bdb34e4bc5
cihat <cihat02020202@gmail.com>
2022-06-05 02:19:40 +0300

    Change routes name to tweet from users

---
+++ backend/src/routes/tweet.js
@@ -0,0 +1,25 @@
+const tweetController = require('../controllers/tweet.js')
+
+const router = require('express').Router()
+
+router.post('/', tweetController.createTweet)
+
+router.get('/', tweetController.getTweets)
+
+// router.get('/', usersController.getUsers)
+
+// router.post('/', usersController.postUser)
+
+// router.delete('/:userId', usersController.deleteUser)
+
+// router.get('/:userId', usersController.getUser)
+
+// router.patch('/:userId/following', usersController.follow)
+
+// router.patch('/:userId', usersController.updateName)
+
+// router.patch('/:userId/like', usersController.like)
+
+// router.patch('/:userId/retweet', usersController.retweet)
+
+module.exports = router
